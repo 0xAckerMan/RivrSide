9eefb04a232a5c44b681bf805db69ca1f1e99c1b
0xAckerMan <joelkores1@gmail.com>
2024-01-17 20:36:46 +0300

    Setted up chi router

---
+++ backend/cmd/api/routes.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/go-chi/chi/v5"
+
+func (app *Application) routes() *chi.Mux{
+    r := chi.NewRouter()
+    r.Route("/api", func(r chi.Router) {
+        r.Get("/healthcheck", app.healthcheck)
+    })
+    return r
+}

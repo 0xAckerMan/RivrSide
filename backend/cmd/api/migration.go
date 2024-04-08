019efd46479fba11266c52be9ebdaa9de62de5ee
0xAckerMan <joelkores1@gmail.com>
2024-04-08 23:56:17 +0300

    chore: Configured gorm auto migrations

---
+++ backend/cmd/api/migration.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/0xAckerMan/internal/data"
+
+
+func (app *Application) migrations() {
+	app.DB.AutoMigrate(
+		&data.Role{},
+        &data.Room{},
+        &data.Payment{},
+        &data.PackagePlan{},
+        &data.User{},
+	)
+}

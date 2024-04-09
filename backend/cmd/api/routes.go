dd750598ce3c296b7953cd19a7f57f1c8c660350
0xAckerMan <joelkores1@gmail.com>
2024-04-09 20:07:00 +0300

    chore: package_plan crud ops route endpoints

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -2,12 +2,30 @@ package main
 
 import "github.com/go-chi/chi/v5"
 
-func (app *Application) routes() *chi.Mux{
-    r := chi.NewRouter()
+func (app *Application) routes() *chi.Mux {
+	r := chi.NewRouter()
+	r.NotFound(app.notFoundResponse)
+    r.MethodNotAllowed(app.methodNotAllowedResponse)
 
-    routes := chi.NewRouter()
-    r.Mount("/api/v1", routes)
-    routes.Get("/healthcheck", app.healthcheck)
-    routes.Get("/packages", app.handle_getAllPackages)
-    return r
+	routes := chi.NewRouter()
+	r.Mount("/api/v1", routes)
+	routes.Get("/healthcheck", app.healthcheck)
+	routes.Group(func(r chi.Router) {
+		r.Get("/packageplans", app.HandleGetAllPackages)
+		r.Post("/packageplan", app.HandleCreatePackagePlan)
+        r.Get("/packageplan/{id}", app.HandleGetSinglePackage)
+        r.Patch("/packageplan/{id}", app.HandleUpdatePackagePlan)
+		r.Delete("/packageplan/{id}", app.HandleDeletePackagePlan)
+	})
+
+    routes.Group(func(r chi.Router) {
+        r.Get("/roles", app.HandleGetAllRole)
+    })
+
+	admin := chi.NewRouter()
+	routes.Mount("/admin", admin)
+	admin.Group(func(r chi.Router) {
+		r.Get("/healthcheck", app.healthcheck)
+	})
+	return r
 }

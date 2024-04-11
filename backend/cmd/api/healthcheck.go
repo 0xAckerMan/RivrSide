c64d4feab97702645354ae56334a95568a3328a1
0xAckerMan <joelkores1@gmail.com>
2024-04-12 02:58:27 +0300

    feat: api landing route message

--- backend/cmd/api/healthcheck.go
+++ backend/cmd/api/healthcheck.go
@@ -16,3 +16,12 @@ func (app *Application) healthcheck (w http.ResponseWriter, r *http.Request) {
         return
     }
 }
+
+func (app *Application) ApiHome (w http.ResponseWriter, r *http.Request){
+    message := "Welcome!!! You made it. Your journey begins here"
+    err := app.writeJSON(w,http.StatusOK,envelope{"success": message},nil)
+    if err != nil{
+        app.serverErrorResponse(w,r,err)
+        return
+    }
+}

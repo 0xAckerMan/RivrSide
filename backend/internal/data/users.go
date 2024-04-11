2da0dd17785012e602c3470396a05102fe6e8039
0xAckerMan <joelkores1@gmail.com>
2024-04-12 02:59:26 +0300

    feat: user structs (tenant,login)

--- backend/internal/data/users.go
+++ backend/internal/data/users.go
@@ -18,13 +18,33 @@ type User struct {
 	Room          *Room        `json:"room" gorm:"foreignKey:RoomID;constraint:OnDelete:CASCADE"`
 	Organisation  string       `json:"organisation"`
 	Position      string       `json:"position"`
-	PackageID     int64        `json:"-"`
-	PackagePlan   *PackagePlan `gorm:"foreignKey:PackageID;constraint:OnDelete:CASCADE"`
-	Month         time.Month   `json:"current_month"`
-	Paymentstatus string       `json:"payment_status"`
-	AmountPaid    int          `json:"amount_paid"`
-	Balance       int          `json:"balance" gorm:"default:0"`
 	Isadmin       bool         `json:"-" gorm:"default:false"`
 	Ismanager     bool         `json:"-" gorm:"default:false"`
 	IsActive      bool         `json:"is_active"`
+}
+
+type CreateTenant struct{
+		First_name    string `json:"first_name"`
+		Last_name     string `json:"last_name"`
+		Email         string `json:"email"`
+		PhoneNumber   string `json:"phone_number"`
+		Password      string `json:"password"`
+		Gender        string `json:"gender"`
+		RoleID        int64  `json:"role"`
+		RoomID        int64  `json:"room"`
+}
+
+type TenantInfo struct {
+    User
+	PackageID     int64        `json:"-"`
+	PackagePlan   *PackagePlan `json:"package_plan"`
+	Month         time.Month   `json:"current_month"`
+	Paymentstatus string       `json:"payment_status"`
+	AmountPaid    int          `json:"amount_paid"`
+	Balance       int          `json:"balance"`
+}
+
+type Login struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }

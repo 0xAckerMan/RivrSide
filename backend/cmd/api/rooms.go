4a87d6d5290948ad41c5886a8854edba91eff462
0xAckerMan <joelkores1@gmail.com>
2024-04-12 03:04:08 +0300

    feat: Updated the all rooms handler to show vaccancies in a room

--- backend/cmd/api/rooms.go
+++ backend/cmd/api/rooms.go
@@ -73,25 +73,30 @@ func (app *Application) HandleGetSingleRoom(w http.ResponseWriter, r *http.Reque
 }
 
 func (app *Application) HandleGetAllRooms(w http.ResponseWriter, r *http.Request) {
-	var rooms []data.Room
+    type RoomInfo struct {
+        data.Room
+        Vacancies int `json:"vacancies"`
+    }
 
-	result := app.DB.Find(&rooms)
+    // Query rooms and calculate vacancies per room
+    var RoomInfos []RoomInfo
+    result := app.DB.
+        Table("rooms").
+        Select("rooms.*, (rooms.capacity - COUNT(users.id)) AS vacancies").
+        Joins("LEFT JOIN users ON rooms.id = users.room_id").
+        Group("rooms.id").
+        Find(&RoomInfos)
+    if result.Error != nil {
+        app.serverErrorResponse(w, r, result.Error)
+        return
+    }
 
-	if result.RowsAffected == 0 {
-		app.noRecordFoundResponse(w, r)
-		return
-	}
+    err := app.writeJSON(w, http.StatusOK, envelope{"rooms": RoomInfos}, nil)
+    if err != nil {
+        app.serverErrorResponse(w, r, err)
+        return
+    }
 
-	if result.Error != nil {
-		app.serverErrorResponse(w, r, result.Error)
-		return
-	}
-
-	err := app.writeJSON(w, http.StatusOK, envelope{"rooms": rooms}, nil)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
 }
 
 func (app *Application) HandlePartialUpdateRoom(w http.ResponseWriter, r *http.Request) {

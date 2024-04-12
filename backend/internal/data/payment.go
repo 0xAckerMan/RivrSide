608b04a20c60d30486ff3a7d254565a196d94924
0xAckerMan <joelkores1@gmail.com>
2024-04-12 03:00:10 +0300

    feat: restructured the struct model schema

--- backend/internal/data/payment.go
+++ backend/internal/data/payment.go
@@ -1,10 +1,15 @@
 package data
 
+import "time"
+
 type Payment struct {
     CommonFields
     UserID int64 `json:"-"`
 	User    *User    `json:"user" gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
-	Amount  int      `json:"Amount"`
+	Amount  int      `json:"Amount_paid"`
+    Balance int `json:"balance"`
     PackageID int `json:"-"`
 	PackagePlan *PackagePlan `json:"package_plan"  gorm:"foreignKey:PackageID;constraint:OnDelete:CASCADE"`
+    Month time.Month `json:"month"`
+    Year time.Time `json:"year"`
 }

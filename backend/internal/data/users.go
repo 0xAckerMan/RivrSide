98436b7c9a212a6e364d861d2d43c9f8cadf446c
0xAckerMan <joelkores1@gmail.com>
2024-04-13 13:37:27 +0300

    feat: manager and tenants structs

--- backend/internal/data/users.go
+++ backend/internal/data/users.go
@@ -14,7 +14,7 @@ type User struct {
 	Gender       string `json:"gender"`
 	RoleID       int64  `json:"-"`
 	Role         *Role  `json:"role" gorm:"foreignKey:RoleID;constraint:OnDelete:CASCADE"`
-	RoomID       int64  `json:"-"`
+    RoomID       int64  `json:"-" gorm:"default:null"`
 	Room         *Room  `json:"room" gorm:"foreignKey:RoomID;constraint:OnDelete:CASCADE"`
 	Organisation string `json:"organisation"`
 	Position     string `json:"position"`
@@ -42,6 +42,31 @@ type TenantInfo struct {
 	Paymentstatus string       `json:"payment_status"`
 	AmountPaid    int          `json:"amount_paid"`
 	Balance       int          `json:"balance"`
+}
+
+type Manager struct {
+	CommonFields
+	First_name   string `json:"first_name" gorm:"not null"`
+	Last_name    string `json:"last_name" gorm:"not null"`
+	Email        string `json:"email" gorm:"uniqueIndex;not null"`
+	PhoneNumber  string `json:"phone_number" gorm:"uniqueIndex;not null"`
+	Password     string `json:"-" gorm:"not null"`
+	Gender       string `json:"gender"`
+	RoleID       int64  `json:"-"`
+	Role         *Role  `json:"role" gorm:"foreignKey:RoleID;constraint:OnDelete:CASCADE"`
+	Organisation string `json:"organisation"`
+	Position     string `json:"position"`
+	Ismanager    bool   `json:"-" gorm:"default:false"`
+	IsActive     bool   `json:"is_active"`
+}
+
+type CreateManager struct {
+	First_name   string `json:"first_name"`
+	Last_name    string `json:"last_name"`
+	Email        string `json:"email"`
+	PhoneNumber  string `json:"phone_number"`
+	Password     string `json:"password"`
+	Gender       string `json:"gender"`
 }
 
 type Login struct {

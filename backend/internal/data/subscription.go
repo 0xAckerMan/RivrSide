609e8bd877dc4f75181de3bc0785777d386a1980
0xAckerMan <joelkores1@gmail.com>
2024-04-16 01:38:19 +0300

    feat: User subscription struct

--- backend/internal/data/subscription.go
+++ backend/internal/data/subscription.go
@@ -2,7 +2,7 @@ package data
 
 type Subscription struct {
 	CommonFields
-    UserID int `json:"-" gorm:"uniqueIndex"`
+    UserID int `json:"user_id" gorm:"uniqueIndex;column:user_id"`
     User *User `json:"user" gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
     PackageID int `json:"-"`
     PackagePlan *PackagePlan `json:"package_plan" gorm:"foreignKey:PackageID;constraint:OnDelete:CASCADE"`

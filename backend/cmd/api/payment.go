0cb0f6364a6033d21b2dbac4aafbd91fea5f57cc
0xAckerMan <joelkores1@gmail.com>
2024-04-16 23:05:52 +0300

    feat: Get all payment records handler

--- backend/cmd/api/payment.go
+++ backend/cmd/api/payment.go
@@ -1,9 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/0xAckerMan/internal/data"
+)
 
 func (app *Application) HandleGetAllPayment  (w http.ResponseWriter, r *http.Request){
-    return
+    var payment []data.Payment
+
+    result := app.DB.Preload("User").Preload("Subscription").Find(&payment)
+    if result.RowsAffected ==0{
+        app.noRecordFoundResponse(w,r)
+        return
+    }
+
+    if result.Error != nil{
+        app.serverErrorResponse(w,r,result.Error)
+        return
+    }
+
+    err := app.writeJSON(w,http.StatusOK,envelope{"all_payments": payment}, nil)
+    if err != nil{
+        app.serverErrorResponse(w,r,err)
+        return
+    }
 }
 
 func (app *Application) HandleGetAllPaymentPerMonth (w http.ResponseWriter, r *http.Request){

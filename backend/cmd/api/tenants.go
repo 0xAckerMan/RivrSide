37da338a45ffe52031dc53832d38ae75644c937b
0xAckerMan <joelkores1@gmail.com>
2024-04-16 22:37:48 +0300

    feat: refactored payment to sum existing amount and calc excess amount

--- backend/cmd/api/tenants.go
+++ backend/cmd/api/tenants.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"database/sql"
 	"errors"
-	"fmt"
 	"net/http"
 	"time"
 
@@ -75,9 +75,8 @@ func (app *Application) HandleUpdateTenantInfo(w http.ResponseWriter, r *http.Re
 
 		currentTenant.Password = string(hash)
 	}
-
+	var subscription data.Subscription
 	if input.Subcription != nil {
-		var subscription data.Subscription
 		if err := app.DB.Unscoped().First(&subscription, "user_id = ?", currentTenant.ID).Error; err != nil {
 			if errors.Is(err, gorm.ErrRecordNotFound) {
 				subscription = data.Subscription{
@@ -123,12 +122,23 @@ func (app *Application) HandleUpdateTenantInfo(w http.ResponseWriter, r *http.Re
 
 	// recheck
 
-    if err = app.DB.Model(&currentTenant).Preload("Room").Preload("Role").First(&currentTenant).Error; err != nil{
+	if err = app.DB.Model(&currentTenant).Preload("Room").Preload("Role").First(&currentTenant).Error; err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+    if err = app.DB.Model(&subscription).Preload("PackagePlan").First(&subscription).Error; err != nil{
         app.serverErrorResponse(w,r,err)
         return
     }
+    
+    var updatedUser = data.TenantInfo{
+        User: currentTenant,
+        PackageID: int64(subscription.PackageID),
+    }
 
-	err = app.writeJSON(w, http.StatusOK, envelope{"tenant": currentTenant}, nil)
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"tenant": updatedUser}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
@@ -167,86 +177,92 @@ func (app *Application) HandleRequestTransfer(w http.ResponseWriter, r http.Requ
 	return
 }
 
-func (app *Application) HandleMakePayment(w http.ResponseWriter, r *http.Request){
-    fmt.Println("Entered the handler")
-    currentTenant := r.Context().Value("user").(data.User)
-    fmt.Println("the current user is", currentTenant)
-    var input data.MakePayment
-    var subscription data.Subscription
+func (app *Application) HandleMakePayment(w http.ResponseWriter, r *http.Request) {
+	currentTenant := r.Context().Value("user").(data.User)
 
-    err := app.readJSON(w,r,&input)
-    if err != nil{
-        app.serverErrorResponse(w,r,err)
+	var input data.MakePayment
+	var subscription data.Subscription
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	res := app.DB.Preload("User").Preload("PackagePlan").Where("user_id = ? AND deleted_at IS NULL", currentTenant.ID).
+		Order("id").Limit(1).First(&subscription)
+	if res.Error == gorm.ErrRecordNotFound {
+		message := "You do not have a package, please select before making payments"
+		app.errorResponse(w, r, http.StatusOK, envelope{"response": message})
+		return
+	}
+
+	if res.Error != nil {
+		app.serverErrorResponse(w, r, res.Error)
+		return
+	}
+
+	var payment data.Payment
+
+	payment = data.Payment{
+		UserID:         int64(currentTenant.ID),
+		Amount:         input.Amount,
+		SubscriptionID: int(subscription.ID),
+		Month:          time.Now().Month(),
+		Year:           time.Now().Year(),
+		Subscription:   &subscription,
+	}
+
+    var totalPaid sql.NullFloat64
+
+    result:= app.DB.Model(&data.Payment{}).
+        Where("user_id = ? AND month = ? AND year = ?", currentTenant.ID, time.Now().Month(), time.Now().Year()).
+        Pluck("SUM(amount)", &totalPaid)
+
+    if result.Error != nil{
+        app.serverErrorResponse(w,r,result.Error)
         return
     }
-
-    // res := app.DB.Where("user_id = ?", currentTenant.ID).First(&subscription)
-    res := app.DB.Preload("User").Preload("PackagePlan").Where("user_id = ? AND deleted_at IS NULL", currentTenant.ID).
-    Order("id").Limit(1).First(&subscription)
-    if res.Error == gorm.ErrRecordNotFound {
-        message := "You do not have a package, please select before making payments"
-        app.errorResponse(w,r,http.StatusOK,envelope{"response": message})
-        return
-    } 
-
-    if res.Error != nil{
-        app.serverErrorResponse(w,r,res.Error)
-        return
+    var totalPaidAmount float64
+    if totalPaid.Valid {
+        totalPaidAmount = totalPaid.Float64
     }
+    totalPaidAmount += float64(input.Amount)
 
-    fmt.Println("Retrieveeed subz", subscription)
+	payment.Balance = subscription.PackagePlan.Price - int(totalPaidAmount)
+	if int(totalPaidAmount) > payment.Subscription.PackagePlan.Price || payment.Balance < 0 {
+		nextMonth := data.Payment{
+			UserID:         int64(currentTenant.ID),
+			Amount:         payment.Amount,
+			SubscriptionID: payment.SubscriptionID,
+			Month:          time.Now().AddDate(0, 1, 0).Month(),
+		}
+		if payment.Year == int(time.December) {
+			nextMonth.Year = time.Now().AddDate(1, 0, 0).Year()
+		} else {
+			nextMonth.Year = time.Now().Year()
+		}
 
-    var payment data.Payment
-    
-    payment = data.Payment{
-        UserID: int64(currentTenant.ID),
-        Amount: input.Amount,
-        SubscriptionID: int(subscription.ID),
-        Month: time.Now().Month(),
-        Year: time.Now().Year(),
-        Subscription: &subscription,
-    }
+		if err = app.DB.Create(&nextMonth).Error; err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+	}
 
+	if err = app.DB.Create(&payment).Error; err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 
-    payment.Balance = subscription.PackagePlan.Price - input.Amount
-    if input.Amount > payment.Subscription.PackagePlan.Price || payment.Balance < 0{
-        nextMonth := data.Payment{
-            UserID: int64(currentTenant.ID),
-            Amount: payment.Amount,
-            SubscriptionID: payment.SubscriptionID,
-            Month: time.Now().AddDate(0,1,0).Month(),
-        }
-        if payment.Year == int(time.December) {
-            nextMonth.Year = time.Now().AddDate(1,0,0).Year()
-        } else {
-            nextMonth.Year = time.Now().Year()
-        }
+	res = app.DB.Model(&payment).Preload("User").Preload("Subscription").First(&payment)
+	if res.Error != nil {
+		app.serverErrorResponse(w, r, res.Error)
+		return
+	}
 
-        if err = app.DB.Create(&nextMonth).Error; err != nil{
-            app.serverErrorResponse(w,r,err)
-            return
-        }
-    }
-
-    fmt.Println("PAYYYY ME INIT", payment)
-
-    if err = app.DB.Create(&payment).Error; err != nil{
-        app.serverErrorResponse(w,r,err)
-        return
-    }
-
-    res = app.DB.Model(&payment).Preload("User").Preload("Subscription").First(&payment) 
-    if res.Error != nil{
-        app.serverErrorResponse(w,r,res.Error)
-        return
-    }
-
-    err = app.writeJSON(w,http.StatusCreated,envelope{"payment": payment},nil)
-    if err != nil{
-        app.serverErrorResponse(w,r,err)
-        return
-    }
-    
-
+	err = app.writeJSON(w, http.StatusCreated, envelope{"payment": payment}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 }
-
